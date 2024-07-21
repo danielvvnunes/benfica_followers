a33d0cfd42b2c2452daff27f214e3bb3be572ccf
TAVARDA <daniel.t.nunes@mercedes-benz.com>
2024-07-21 20:57:54 +0100

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -10,5 +12,6 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );

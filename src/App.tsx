b40e13f0e478bb342184f4e9f78818a93e31fa09
TAVARDA <daniel.t.nunes@mercedes-benz.com>
2024-07-22 23:24:19 +0100

    feat: add new level

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,22 @@ import Box from "@mui/material/Box";
 import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Divider } from "@mui/material";
+import { CardActionArea } from "@mui/material";
 import CountUp from "react-countup";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
+import Countdown from "react-countdown";
+import { useMediaQuery } from "react-responsive";
 
 const App = () => {
-  interface User {
+  interface Player {
     name: string;
     image: string;
     followers: number;
   }
 
-  const [players, setPlayers] = useState<User[]>([]);
-  const [player1, setPlayer1] = useState<User | null>(null);
-  const [player2, setPlayer2] = useState<User | null>(null);
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [randomPlayers, setRandomPlayers] = useState<Player[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isClicked, setIsClicked] = useState(false);
   const [isRight, setIsRight] = useState(false);
@@ -29,6 +30,11 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [length, setLength] = useState(0);
+  const [level, setLevel] = useState(2);
+
+  const isSmall = useMediaQuery({ maxWidth: 767 });
+  // const isMedium = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+  // const isLarge = useMediaQuery({ minWidth: 1024 });
 
   const style = {
     position: "absolute" as "absolute",
@@ -41,36 +47,70 @@ const App = () => {
     p: 4,
   };
 
-  function getRandomNumber(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  useEffect(() => {
+    console.log("mudei o level", level);
+  }, [level]);
+
+  useEffect(() => {
+    const numbers: number[] = generateRandomNumbers(1, 4, level);
+
+    const randomPlayers = numbers.map((number) => players[number]);
+    console.log("Tentativa 1", randomPlayers);
+    setRandomPlayers(randomPlayers);
+
+    setIsModalOpen(false);
+    setIsClicked(false);
+    setHasResult(false);
+  }, [level, players]);
+
+  function newGame() {
+    const numbers: number[] = generateRandomNumbers(1, length - 1, 2);
+
+    const randomPlayers = numbers.map((number) => players[number]);
+    setRandomPlayers(randomPlayers);
+
+    setScore(0);
+    setLevel(2);
+    setIsModalOpen(false);
   }
 
   function restartGame() {
-    const [randomNumber1, randomNumber2] = generateTwoDifferentNumbers(
+    const numbers: number[] = generateRandomNumbers(
       1,
-      length - 1
+      length - 1,
+      score >= 2 ? 4 : 2
     );
 
-    setPlayer1(players[randomNumber1]);
-    setPlayer2(players[randomNumber2]);
+    const randomPlayers = numbers.map((number) => players[number]);
+    console.log("Tentativa 2", randomPlayers);
+    setRandomPlayers(randomPlayers);
 
     setIsModalOpen(false);
-    setScore(0);
     setIsClicked(false);
     setHasResult(false);
   }
 
-  function generateTwoDifferentNumbers(min: number, max: number) {
-    // Generate the first random number
-    const num1: number = getRandomNumber(min, max - 1);
+  function generateRandomNumbers(
+    min: number,
+    max: number,
+    level: number
+  ): number[] {
+    if (level > max - min + 1) {
+      throw new Error(
+        "O valor de level é maior do que o intervalo disponível."
+      );
+    }
 
-    // Generate the second random number ensuring it's different from the first
-    let num2;
-    do {
-      num2 = getRandomNumber(min, max);
-    } while (num1 === num2);
+    const numbers = new Set();
 
-    return [num1, num2];
+    // Gera números aleatórios únicos até o nível desejado
+    while (numbers.size < level) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      numbers.add(randomNumber);
+    }
+
+    // Converte o Set para um array e retorna
+    return Array.from(numbers) as number[];
   }
 
   useEffect(() => {
@@ -87,14 +127,15 @@ const App = () => {
 
         setPlayers(data);
 
-        // Set the initial players
-        const [randomNumber1, randomNumber2] = generateTwoDifferentNumbers(
-          1,
-          length
-        );
+        const numbers: number[] = generateRandomNumbers(1, length - 1, level);
 
-        setPlayer1(data[randomNumber1]);
-        setPlayer2(data[randomNumber2]);
+        console.log("numbers", numbers);
+
+        const randomPlayers = numbers.map((number) => data[number]);
+        console.log("Tentativa 3", randomPlayers);
+        setRandomPlayers(randomPlayers);
+
+        console.log("randomPlayers", randomPlayers);
 
         setIsLoading(false);
       } catch (error) {
@@ -105,40 +146,65 @@ const App = () => {
     fetchData();
   }, []);
 
-  const handleClick = (number1: User, number2: User) => {
-    console.log("number1", number1);
+  const handleClick = (player: Player) => {
+    // Encontra o valor máximo da chave 'followers'
+    const maxFollowers = Math.max(
+      ...randomPlayers.map((player) => player.followers)
+    );
 
-    if (number1.followers > number2.followers) {
-      setIsRight(true);
-    } else if (number1.followers < number2.followers) {
-      setIsRight(false);
-    }
+    const result = player.followers === maxFollowers;
+
+    setIsRight(result);
 
     setIsClicked(true);
 
     setTimeout(() => {
       setHasResult(true);
-      if (number1.followers > number2.followers) {
+      if (result) {
         setScore(score + 1);
       }
     }, 3000);
 
     setTimeout(() => {
-      if (number1.followers < number2.followers) {
-        setIsModalOpen(true);
-      }
-
-      const [randomNumber1, randomNumber2] = generateTwoDifferentNumbers(
-        1,
-        length - 1
-      );
-
-      setPlayer1(players[randomNumber1]);
-      setPlayer2(players[randomNumber2]);
-
-      setIsClicked(false);
-      setHasResult(false);
+      result ? restartGame() : gameOver();
     }, 4000);
+  };
+
+  function gameOver() {
+    setIsModalOpen(true);
+
+    const numbers: number[] = generateRandomNumbers(
+      1,
+      players.length - 1,
+      level
+    );
+
+    const randomPlayers = numbers.map((number) => players[number]);
+    setRandomPlayers(randomPlayers);
+
+    setIsClicked(false);
+    setHasResult(false);
+  }
+
+  const renderer = ({
+    seconds,
+    completed,
+  }: {
+    seconds: number;
+    completed: boolean;
+  }) => {
+    if (completed) {
+      // Render a completed state when the countdown finishes
+      gameOver();
+      return <span>Time's up!</span>;
+    } else {
+      // Render the countdown timer
+      return (
+        <div className="w-16 h-16 bg-red-500 text-white rounded-[50%] flex items-center justify-center text-lg font-semibold shadow-lg">
+          {seconds}
+        </div>
+      );
+    }
   };
 
   return (
@@ -148,10 +214,9 @@ const App = () => {
       ) : (
         <>
           <header>
-            <div className="px-4 pt-4 flex justify-center items-center">
+            <div className="px-4 pt-4 flex justify-center items-center ">
               <img src="/vv_logo.png" width={200} height={200} />
             </div>
-            {/* <Divider /> */}
           </header>
           <div className="flex flex-grow flex-col h-full items-center justify-center p-4">
             {hasResult && (
@@ -169,78 +234,54 @@ const App = () => {
             )}
 
             <div className="w-full p-7 flex flex-col items-center justify-center text-center">
-              <Typography
-                gutterBottom
-                variant="h2"
-                component="div"
-                className="hidden sm:block md:block lg:block xl:block"
-              >
-                Who has more followers?
-              </Typography>
+              <div className="flex gap-3">
+                <Typography
+                  gutterBottom
+                  variant={isSmall ? "h4" : "h2"}
+                  component="div"
+                >
+                  Who has more followers?
+                </Typography>
 
-              <Typography
-                gutterBottom
-                variant="h4"
-                component="div"
-                className="sm:hidden"
-              >
-                Who has more followers?
-              </Typography>
+                {/* <img src="/instagram_logo.webp" width={20} height={20} /> */}
+              </div>
 
-              <img src="/instagram_logo.webp" width={60} height={60} />
+              <Countdown
+                date={Date.now() + 20000} // 10 seconds from now
+                renderer={renderer}
+              />
             </div>
 
-            <div className="flex w-full  justify-center gap-4">
-              <Card className="min-h-44 md:w-[400px] transition-transform duration-1000 ease-in-out transform  hover:scale-[102%]">
-                <CardActionArea
-                  onClick={() => {
-                    handleClick(player1, player2);
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    height="210"
-                    image={player1 ? player1.image : undefined}
-                  />
-                  <CardContent className="flex justify-center text-center flex-col items-center">
-                    <Typography gutterBottom variant="h5" component="div">
-                      {player1 && player1.name}
-                    </Typography>
-                    {isClicked && (
-                      <Typography variant="h5" color="text.secondary">
-                        {player1 && (
-                          <CountUp end={player1.followers} duration={3} />
+            <div className="grid grid-cols-2 gap-4">
+              {randomPlayers.map((player) => {
+                return (
+                  <Card className="min-h-44 md:w-[300px] transition-transform duration-1000 ease-in-out transform  hover:scale-[102%]">
+                    <CardActionArea
+                      onClick={() => {
+                        handleClick(player);
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        height="210"
+                        image={player ? player.image : undefined}
+                      />
+                      <CardContent className="flex justify-center text-center flex-col items-center">
+                        <Typography gutterBottom variant="h5" component="div">
+                          {player && player.name}
+                        </Typography>
+                        {isClicked && (
+                          <Typography variant="h5" color="text.secondary">
+                            {player && (
+                              <CountUp end={player.followers} duration={3} />
+                            )}
+                          </Typography>
                         )}
-                      </Typography>
-                    )}
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-              <Card className="transition-transform duration-1000 ease-in-out transform  hover:scale-[102%] min-h-44 md:w-[400px]">
-                <CardActionArea
-                  onClick={() => {
-                    handleClick(player2, player1);
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    height="210"
-                    image={player2 ? player2.image : undefined}
-                  />
-                  <CardContent className="flex justify-center text-center flex-col items-center">
-                    <Typography gutterBottom variant="h5" component="div">
-                      {player2 && player2.name}
-                    </Typography>
-                    {isClicked && (
-                      <Typography variant="h5" color="text.secondary">
-                        {player2 && (
-                          <CountUp end={player2.followers} duration={3} />
-                        )}
-                      </Typography>
-                    )}
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                );
+              })}
             </div>
             <div className="flex items-center justify-center py-6">
               <Typography gutterBottom variant="h5" component="div">
@@ -274,7 +315,7 @@ const App = () => {
                   >
                     {score}
                   </Typography>
-                  <Button onClick={restartGame} variant="contained">
+                  <Button onClick={newGame} variant="contained">
                     Play again
                   </Button>
                 </Box>
